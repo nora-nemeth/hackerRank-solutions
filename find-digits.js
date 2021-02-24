2375cc700972004939cf2aa79bf9f64765f9fcd9
nora-nemeth <nemethnora1996@gmail.com>
2021-02-24 11:14:05 +0100

    Simplified find digits solution

--- find-digits.js
+++ find-digits.js
@@ -3,11 +3,9 @@
 'use strict';
 
 function findDigits(number) {
-  let digits = String(number).split('');
-
   let divisors = 0;
 
-  for (const digit of digits) {
+  for (const digit of String(number)) {
     if (number % digit == 0) {
       divisors++;
     }  
