201f98fba7ff86df3ca020b20bf86e1fcbedae51
nora-nemeth <nemethnora1996@gmail.com>
2021-02-24 15:17:31 +0100

    Upload mars exploration's other solutions

--- mars-exploration.js
+++ mars-exploration.js
@@ -6,14 +6,48 @@ function marsExploration(signal) {
   let change = 0;
 
   for (let i = 0; i < signal.length; i += 3) {
-    if (signal[i] !== "S") {
+    if (signal[i] !== 'S') {
       change++;
-    } else if (signal[i + 1] !== "O") {
+    }
+    if (signal[i+1] !== 'O') {
       change++;
-    } else if (signal[i + 2] !== "S") {
+    }
+    if (signal[i+2] !== 'S') {
       change++;
     }
   }
 
   return change;
+}
+
+
+//other solution
+
+
+function marsExploration(signal) {
+  let change = 0;
+  
+  const pattern = ['S', 'O', 'S'];
+
+  for (let i = 0; i < signal.length; ++i) {
+    if (signal[i] !== pattern[i % 3]) {
+      change++;
+    }
+  }
+
+  return change;
+}
+
+
+//funtional solution
+
+
+function countAlterations(signal, pattern) {
+  const isAltered = (char, index) => char !== pattern[index % pattern.length];
+  return signal.split('').filter(isAltered).length;
+}
+
+function marsExploration(signal) {
+  const pattern = ['S', 'O', 'S'];
+  return countAlterations(signal, pattern);
 }
